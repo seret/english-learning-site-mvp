443ae9bda13243f8cd3c5eb3c406d32f898cb1bb
Maxim Gilyadov <gmax82@gmail.com>
2026-04-02 20:59:48 +0300

    Add components index file

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+# Export all components from the components folder
+export * from './button';
+export * from './input';
+export * from './card';
+export * from './modal';
+export * from './toast';
+export * from './loader';
+export * from './tabs';
+export * from './accordion';
+export * from './progressbar';
+export * from './badge';
+export * from './textarea';
+export * from './select';
+export * from './alert';
